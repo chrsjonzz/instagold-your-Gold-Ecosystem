e45397318008c496146108e839d99fdd63d71fd6
Sharon R <chrsjonzz74@gmail.com>
2025-11-07 22:28:38 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/toaster"
 import CursorPhoenix from "@/components/CursorPhoenix";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import { Providers } from './providers';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <Providers>
           <CursorPhoenix />
           <div className="flex flex-col min-h-screen">
               <Header />
@@ -32,6 +34,7 @@ export default function RootLayout({
               <Footer />
           </div>
           <Toaster />
+        </Providers>
       </body>
     </html>
   );

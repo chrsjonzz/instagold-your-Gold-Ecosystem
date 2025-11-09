08679925bb84acdb6a5f42f444399072fd051ab8
Sharon R <chrsjonzz74@gmail.com>
2025-11-09 17:59:44 +0000

    try from out colour that match our theme for the BG Its not attractive A

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased bg-gradient-to-b from-background to-[#111827]">
           <CursorPhoenix />
           <div className="flex flex-col min-h-screen">
               <Header />
